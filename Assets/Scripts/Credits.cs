7543d6571ff005806a909d61d9d4c4547b87d8b6
Don Juan Javier <townofdon@gmail.com>
2020-10-13 22:38:23 -0400

    add menu and credits scenes

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
